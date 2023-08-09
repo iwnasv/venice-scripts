8356624dfe2b23f5fb19fd15e3de69ee21718a8e
iwnasv <34813745+iwnasv@users.noreply.github.com>
2023-08-09 12:04:00 +0300

    add -e (equal match) flag

--- count-items.py
+++ count-items.py
@@ -5,7 +5,7 @@ query_conditions = "FROM cwf_pooltask as p_task " \
                    "JOIN cwf_workflowitem as w_item ON w_item.workflowitem_id = p_task.workflowitem_id " \
                    "JOIN metadatavalue as identifier ON identifier.dspace_object_id = w_item.item_id " \
                    "AND metadata_field_id = 24 " \
-                   "WHERE identifier.text_value LIKE %s " # TODO opt arg to toggle LIKE/EQUAL
+                   "WHERE identifier.text_value {} %s "
 
 def connect_to_database():
     return psycopg2.connect(
@@ -16,8 +16,9 @@ def connect_to_database():
         database="dspace"
     )
 
-def get_task_count(pattern, cursor):
-    count_query = "SELECT COUNT(*) " + query_conditions
+def get_task_count(pattern, cursor, equal=False):
+    condition = "=" if equal else "LIKE"
+    count_query = "SELECT COUNT(*) " + query_conditions.format(condition)
 
     cursor.execute(count_query, (pattern,))
     count = cursor.fetchone()[0]
@@ -26,9 +27,11 @@ def get_task_count(pattern, cursor):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("identifier_pattern", help="Pattern to search for in task identifiers")
+parser.add_argument("-e", "--equal", action="store_true", help="Toggle EQUAL query instead of LIKE")
 args = parser.parse_args()
 
 identifier_pattern = args.identifier_pattern
+use_equal = args.equal
 
 connection = None
 cursor = None
@@ -37,7 +40,7 @@ try:
     connection = connect_to_database()
     cursor = connection.cursor()
 
-    task_count = get_task_count(identifier_pattern, cursor)
+    task_count = get_task_count(identifier_pattern, cursor, use_equal)
     print(task_count)
 
 except psycopg2.Error as error:
